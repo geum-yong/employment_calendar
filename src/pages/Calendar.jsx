54f951732865d0b7bf0ba7e868094306077cadee
geum-yong <gmyong21@gmail.com>
2021-11-02 14:06:59 +0900

    Remove: container 컴포넌트 삭제

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,7 +1,19 @@
-import Container from '../components/Container';
+import styled from 'styled-components';
+import CalendarGrid from '../components/CalendarGrid';
+import CalendarHeader from '../components/CalendarHeader';
+
+const Container = styled.div`
+  margin: 0 auto;
+  width: 1000px;
+`;
 
 const Calendar = () => {
-  return <Container>testtest</Container>;
+  return (
+    <Container>
+      <CalendarHeader />
+      <CalendarGrid />
+    </Container>
+  );
 };
 
 export default Calendar;
